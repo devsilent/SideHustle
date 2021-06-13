bd26a0f6a12924ba777a8a62d6252c0a72af66c7
abc <motocapstone2021@gmail.com>
2021-06-13 20:59:52 +0800

    Added Login activity and layout

---
+++ app/src/main/java/com/silentdev/sidehustle/ui/Login.java
@@ -0,0 +1,16 @@
+package com.silentdev.sidehustle.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.silentdev.sidehustle.R;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_activity);
+    }
+}
